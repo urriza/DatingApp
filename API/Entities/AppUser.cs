07531ddeeec7c7af30a443859f9e6ea16824f07f
Iosu Urriza Rivera <i.urriza@i3code.es>
2021-05-12 15:05:46 +0200

    + Añadir prppiedades clase AppUser

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace API.Entities
+﻿namespace API.Entities
 {
     public class AppUser
     {
+        public int Id{ get; set; }
+        public string UserName{ get; set; }
+
     }
 }
