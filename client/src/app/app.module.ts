bb0cc3c8663c90796a310d827ba229c89de9a30a
Iosu Urriza Rivera <i.urriza@i3code.es>
2021-08-12 14:00:30 +0200

    ~ Avances seccion 7

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { RegisterComponent } from './register/register.component';
 import { SharedModule } from './_modules/shared.module';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 
 
@@ -21,7 +22,8 @@ import { ErrorInterceptor } from './_interceptors/error.interceptor';
     NavComponent,
     HomeComponent,
     RegisterComponent,
-    TestErrorsComponent
+    TestErrorsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
