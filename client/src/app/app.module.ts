10a9995a58d9c4a1b04094fd79d2260369fa9d81
Iosu Urriza Rivera <i.urriza@i3code.es>
2021-08-13 07:27:05 +0200

    ~ Terminar seccion 7

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { SharedModule } from './_modules/shared.module';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
 
 
 
@@ -23,7 +24,8 @@ import { NotFoundComponent } from './errors/not-found/not-found.component';
     HomeComponent,
     RegisterComponent,
     TestErrorsComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    ServerErrorComponent
   ],
   imports: [
     BrowserModule,
